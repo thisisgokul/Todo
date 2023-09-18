a81d5b4fbce98413a3b28e5a72a0cd9f2f9d5261
thisisgokul <gokulgs4923@gmail.com>
2023-09-18 18:04:18 +0530

    fs

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // Enable CORS 
 app.use(cors({
-    origin: 'http://127.0.0.1:5173', 
+    origin: 'https://todo-eight-gilt.vercel.app', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   }));
